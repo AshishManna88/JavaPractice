991ecff91ec2d762fc028334b83e5d8f6f6e9384
AshishManna88 <ashishmanna88@gmail.com>
2024-04-22 11:33:45 +0530

    Update PaswordCkeckerWithDoWhile.java

--- PaswordCkeckerWithDoWhile.java
+++ PaswordCkeckerWithDoWhile.java
@@ -1,20 +1,16 @@
 import java.util.Scanner;
 public class PaswordCkeckerWithDoWhile {
     public static void main(String[] args) {
-        boolean passCheck;
+        int num = 5;
         int i = 0;
+        Scanner st = new Scanner(System.in);
+        String inputNumber = "";
         do {
-            Scanner st = new Scanner(System.in);
-            String inputPassword = st.nextLine();
-            passCheck = passwordChecker(inputPassword);
-            if (passCheck) {
-                System.out.println("Correct Password");
-                break;
-            } else {
-                System.out.println("wrong password");
-                i++;
-            }
-        } while (true);
+            System.out.println("Enter a Password: ");
+            inputNumber = st.nextLine();
+            System.out.println("Pasword is" + (passwordChecker(inputNumber) ? " correct": " wrong"));
+            i++;
+        } while (!passwordChecker(inputNumber));
         
     }
     public static boolean passwordChecker(String inputPassword){
