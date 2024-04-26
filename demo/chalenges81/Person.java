1cb082d5a62b286806ffd1de52b202a23b38622d
AshishManna88 <ashishmanna88@gmail.com>
2024-04-26 22:21:58 +0530

    Equals and Hashcode

---
+++ demo/chalenges81/Person.java
@@ -0,0 +1,47 @@
+package demo.chalenges81;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + age;
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (age != other.age)
+            return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person("Ashish", 30);
+        Person person1 = new Person("Ashish", 30);
+
+        System.out.println((person.equals(person1))? "Equals":"Not Equals");
+
+
+    }
+}
