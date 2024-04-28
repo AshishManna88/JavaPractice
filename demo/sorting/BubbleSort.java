845e9fe7ad7433492600383089873a295db65687
AshishManna88 <ashishmanna88@gmail.com>
2024-04-29 00:30:19 +0530

    bubble sort

--- demo/sorting/BubbleSort.java
+++ demo/sorting/BubbleSort.java
@@ -1,4 +1,5 @@
 package demo.sorting;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BubbleSort {
@@ -8,7 +9,16 @@ public class BubbleSort {
         System.out.println("Enter size of Array");
         int arraySize = st.nextInt();
         int[] newArray = getArray(arraySize);
+
+        //Arrays.sort(newArray);
+        //System.out.println(Arrays.toString(newArray));
+
+        System.out.println("\nYour Array is: ");
         printArray(newArray);
+        
+        System.out.println("\nYour Sorted Array is: ");
+        int[] sortedArray = sortArray(newArray);
+        printArray(sortedArray);
 
     }
     static int[] getArray(int arraySize){
@@ -22,8 +32,34 @@ public class BubbleSort {
     }
 
     static void printArray(int[] newArray){
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.print(newArray[i]);
+            if (i < newArray.length - 1) {
+                System.out.print(",");
+            }
+        }
+
+        boolean first = true;
         for (int i : newArray) {
-            System.out.print(i + " ");
+            if (!first) {
+                System.out.print(",");
+            } else {
+                first = false;
+            }
+            System.out.print(i);
         }
     }
+
+    static int[] sortArray(int[] newArray){
+        for (int i = 0; i < newArray.length-1; i++) {
+            for (int j = i+1; j < newArray.length; j++) {
+                if(newArray[i] > newArray[j]){
+                    newArray[i] = newArray[i] + newArray[j]; //a = a+b; a=10, b=20
+                    newArray[j] = newArray[i] - newArray[j]; //b = a-b 30-20
+                    newArray[i] = newArray[i] - newArray[j]; //a = a - b 30-10
+                }
+            }
+        }
+        return newArray;
+    } 
 }
